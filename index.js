34a1c83bae3e4ec0cceed20f8d53942eb9ff1a91
SabbirAhamedMaruf <sabbirahamedmaruf02@gmail.com>
2023-12-22 12:05:44 +0600

    added todo fetching api and delete todo api

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pzharqa.mongodb.net/?retryWrites=true&w=majority`;
-
 // Mongodb URI (MongoDB Cred)
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pzharqa.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,10 +24,54 @@ async function run() {
     const database = client.db("Todolist");
     const todoCollection = database.collection("todos");
 
-    console.log(
-      "Log: Connection established with database!"
-    );
-  } finally {}
+    // API
+    // ! GET APIS
+    // getting all todos
+    app.get("/gettodo", async (req, res) => {
+      const todosData = await todoCollection.find().toArray();
+      res.send(todosData);
+    });
+    // getting todos data by pagination
+    app.get("/gettodobypagination", async (req, res) => {
+      const currentPage = req.query.currentPage;
+      const priority = req.query.priority;
+      if (priority === "none") {
+        const todosData = await todoCollection.find().toArray();
+        res.send(todosData);
+      } else {
+        const query = { priority: priority };
+        const todosData = await todoCollection
+          .find(query)
+          .skip(parseInt(currentPage))
+          .limit(10)
+          .toArray();
+        res.send(todosData);
+      }
+    });
+
+    // ! POST APIS
+    // adding todo
+    app.post("/addtodo", async (req, res) => {
+      const currentTodoData = req.body;
+      const result = await todoCollection.insertOne(currentTodoData);
+      res.send(result);
+    });
+
+    // ! DELETE APIS
+    app.delete("/deletetodo", async (req, res) => {
+      const currentTodoId = req.query.id;
+      const query = { _id: new ObjectId(currentTodoId)};
+      const result = await todoCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+
+
+    // ! Loging mongodb connection
+    console.log("Log: Connection established with database!");
+  } finally {
+  }
 }
 run().catch(console.dir);
 
